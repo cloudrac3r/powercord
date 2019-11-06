0b4f4fb85a97c1a0aefcc51ddd1921efe87fa2db
Cadence Fish <cloudrac3r@vivaldi.net>
2019-11-06 22:24:19 +1300

    Revert "merge iv + salt in 1 key"
    This reverts commit bbc096fbfb98417a4d8499fea66cd26bf9d7aa30.

--- src/Powercord/plugins/pc-april/encryptor.js
+++ src/Powercord/plugins/pc-april/encryptor.js
@@ -16,7 +16,8 @@ const encryptor = {
     // IV + salt
     const iv = randomBytes(16);
     const salt = randomBytes(32);
-    await writeFile(resolve(this.path, 'keys.enc'), Buffer.concat([ iv, salt ]));
+    await writeFile(resolve(this.path, 'iv.enc'), iv);
+    await writeFile(resolve(this.path, 'salt.enc'), salt);
 
     // Key
     const key = scryptSync(passphrase, salt, 32);
@@ -33,13 +34,12 @@ const encryptor = {
 
   async decrypt (passphrase) {
     const memes = [];
-    const keys = await readFile(resolve(this.path, 'keys.enc'));
-    const iv = keys.slice(0, 16);
-    const salt = keys.slice(16, 48);
+    const iv = await readFile(resolve(this.path, 'iv.enc'));
+    const salt = await readFile(resolve(this.path, 'salt.enc'));
     const key = scryptSync(passphrase, salt, 32);
     const files = await readdir(this.path);
     for (const file of files) {
-      if (file === 'keys.enc') {
+      if ([ 'iv.enc', 'salt.enc' ].includes(file)) {
         continue;
       }
 
